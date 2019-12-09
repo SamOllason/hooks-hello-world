2aac524a63ac82dc24a113c4c9da7277a8396696
Samuel Ollason <samuelollason@Samuels-MacBook-Pro-2.local>
2019-12-09 11:06:14 +0000

    Updates

---
+++ precache-manifest.bf40c0e2906f176d037fd634e824e49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42684fd0afefd6a92f16b08371943c6a",
+    "url": "/hooks-hello-world/index.html"
+  },
+  {
+    "revision": "1205e08eb1bed8f0d6f5",
+    "url": "/hooks-hello-world/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b451bffde3dba189707b",
+    "url": "/hooks-hello-world/static/js/2.9a26e1fc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hooks-hello-world/static/js/2.9a26e1fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1205e08eb1bed8f0d6f5",
+    "url": "/hooks-hello-world/static/js/main.dcbc600b.chunk.js"
+  },
+  {
+    "revision": "3aa8ebf17bf30f1fef55",
+    "url": "/hooks-hello-world/static/js/runtime-main.2ba11fb7.js"
+  }
+]);
